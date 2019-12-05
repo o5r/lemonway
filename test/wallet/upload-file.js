601b9f09eef17116e1ebea5432e3af3ce04877e4
Sh0ri <>
2019-12-05 15:08:35 +0100

    lint

--- test/wallet/upload-file.js
+++ test/wallet/upload-file.js
@@ -11,7 +11,24 @@ describe('upload file', function () {
   this.timeout(2000000);
 
   it('upload a file', function () {
-    var lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT);
+    const lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT);
+    return lemonway.Wallet.create(chance.ip(), {
+      id: chance.word({ syllables: 5 }),
+      email: chance.email(),
+      firstName: chance.first(),
+      lastName: chance.last(),
+      birthDate: new Date()
+    }).then(function (wallet) {
+      return wallet.uploadFile(chance.ip(), {
+        fileName: 'RIB.png',
+        type: 'RIB',
+        filePath: './test/wallet/RIB.png'
+      });
+    });
+  });
+
+  it('upload all kyc files', async() => {
+    const lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT);
     return lemonway.Wallet.create(chance.ip(), {
       id: chance.word({ syllables: 5 }),
       email: chance.email(),
