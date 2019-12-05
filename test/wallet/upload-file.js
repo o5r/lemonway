8ce72189ce00810f9b92794a11f02c4075a8ff48
Sh0ri <>
2019-12-05 15:51:40 +0100

    removed test

--- test/wallet/upload-file.js
+++ test/wallet/upload-file.js
@@ -26,21 +26,4 @@ describe('upload file', function () {
       });
     });
   });
-
-  it('upload all kyc files', async() => {
-    const lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT);
-    return lemonway.Wallet.create(chance.ip(), {
-      id: chance.word({ syllables: 5 }),
-      email: chance.email(),
-      firstName: chance.first(),
-      lastName: chance.last(),
-      birthDate: new Date()
-    }).then(function (wallet) {
-      return wallet.uploadFile(chance.ip(), {
-        fileName: 'RIB.png',
-        type: 'RIB',
-        filePath: './test/wallet/RIB.png'
-      });
-    });
-  });
 });
