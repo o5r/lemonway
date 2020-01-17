a8c3f47c43c1228dfff9620a63ff77e97a3306a6
Quentin Oliveira Da Silva <da-silva@october.eu>
2020-01-17 17:34:29 +0100

    updated and tested createIban function

---
+++ test/wallet/createIban.js
@@ -0,0 +1,36 @@
+const expect = require('chai').expect;
+const Chance = require('chance');
+const moment = require('moment');
+
+const Lemonway = require('../../');
+
+const chance = new Chance();
+
+describe.only('register iban', function () {
+  this.timeout(2000000);
+
+  it('should create an iban',async() => {
+    var lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT);
+    const walletOpts = {
+      id: chance.word({ syllables: 5 }),
+      email: chance.email(),
+      firstName: chance.first(),
+      lastName: chance.last(),
+      country: 'FRA',
+      birthdate: moment().format('DD/MM/YYYY'),
+      isCompany: false,
+      nationality: 'FRA',
+      payerOrBeneficiary: true,
+    };
+
+    const createdWallet = await lemonway.Wallet.create(chance.ip(), walletOpts);
+
+    const lemonwayIban = await createdWallet.createIBAN(chance.ip(), {
+      wallet: createdWallet.id,
+      country: 'FR'
+    });
+
+    expect(lemonwayIban.id).to.not.be.undefined;
+  });
+
+});
