c8469aa204c21f451742f7d84e12dac95e4a87d9
Julien Guepin <guepin@october.eu>
2020-03-24 11:54:02 +0100

    Some tests for cancelMoneyIn

--- test/wallet/money-in-sdd.js
+++ test/wallet/money-in-sdd.js
@@ -11,16 +11,29 @@ var chance = new Chance();
 
 describe('money in sdd', function () {
   this.timeout(2000000);
+  var lemonway = new Lemonway(
+    process.env.LOGIN,
+    process.env.PASS,
+    process.env.ENDPOINT,
+    process.env.WK_URL,
+    { proxy: process.env.LEMONWAY_PROXY_URL }
+  );
+  var mandate;
+  var id;
+  var transaction;
 
   it('credit a wallet', function (done) {
-    var lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT, process.env.WK_URL);
-    var id = chance.word({ syllables: 5 });
+    id = chance.word({ syllables: 5 });
     lemonway.Wallet.create(chance.ip(), {
       id: id,
       email: chance.email(),
       firstName: chance.first(),
       lastName: chance.last(),
-      birthDate: new Date()
+      birthDate: new Date(),
+      country: 'FRA',
+      nationality: 'FRA',
+      payerOfBeneficiary: '1',
+      isCompany: false
     }).then(function (wallet) {
       return wallet.updateWalletStatus(chance.ip(), {
         status: 'KYC_2'
@@ -31,7 +44,9 @@ describe('money in sdd', function () {
           iban: 'FR1420041010050500013M02606',
           isRecurring: false
         });
-      }).then(function (mandate) {
+      }).then(function (_mandate) {
+        mandate = _mandate;
+
         return mandate.signDocumentInit(chance.ip(), wallet, {
           mobileNumber: process.env.PHONE,
           returnUrl: chance.url(),
@@ -50,10 +65,23 @@ describe('money in sdd', function () {
         });
       });
     }).then(function (transaction) {
+      transaction = transation.id;
+
       return done();
     }).catch((err) => {
       console.log(err); // Url returned by lemonway on the dev environment fail to validate SSD Mandate
       done();
     });
   });
+
+  it('cancel credit a wallet', function (done) {
+    return lemonway.Wallet.cancelMoneyIn(chance.ip(), id, transaction)
+      .then(function (transaction) {
+
+        return done();
+
+      }).catch((err) => {
+        done(err);
+      });
+    });
 });
