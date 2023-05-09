31c59446806a492c81f833e0b5c1897a8597cbc2
Cedric Louyot <cedric.louyot@gmail.com>
2023-05-09 13:28:25 +0200

    add holder renaming test

--- test/wallet/register-iban.js
+++ test/wallet/register-iban.js
@@ -1,42 +1,58 @@
 'use strict';
 
-var expect = require('chai').expect;
-var Chance = require('chance');
+const expect = require('chai').expect;
+const Chance = require('chance');
 
-var Lemonway = require('../../');
+const Lemonway = require('../../');
 
-var chance = new Chance();
+const chance = new Chance();
 
-describe.only('register iban', function () {
+describe('register iban', function () {
   this.timeout(2000000);
 
-  it('registers an iban with authorized chars in holder field', function (done) {
-    var lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT);
-    lemonway.Wallet.get(chance.ip(), {
-      id: '106',
-      email: 'jean.dupont106@mail.com',
-    }).then(function (response) {
-      return response.wallet.registerIBAN(chance.ip(), {
-        holder: 'alphanumeric 123 and - only',
-        iban: 'FR1420041010050500013M02606'
-      });
-    }).then(function (iban) {
-      return done();
-    }).catch(done);
+  const lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT, process.env.WK_URL);
+  const ip = chance.ip();
+  let newWallet;
+
+  before(async function() {
+    newWallet = await lemonway.Wallet.create(ip, {
+      id: chance.word({ syllables: 5 }),
+      email: chance.email(),
+      firstName: chance.first(),
+      lastName: chance.last(),
+      birthdate: '25/05/1980',
+      payerOrBeneficiary: '1',
+      country: 'FRA',
+      isCompany: false,
+      nationality: 'FRA',
+    });
   });
 
-  it('registers an iban with unauthorized chars in holder changed in spaces', function (done) {
-    var lemonway = new Lemonway(process.env.LOGIN, process.env.PASS, process.env.ENDPOINT);
-    lemonway.Wallet.get(chance.ip(), {
-      id: '106',
-      email: 'jean.dupont106@mail.com',
-    }).then(function (response) {
-      return response.wallet.registerIBAN(chance.ip(), {
-        holder: 'unauthorized chars like _ and &.',
-        iban: 'FR1420041010050500013M02606'
-      });
-    }).then(function (iban) {
-      return done();
-    }).catch(done);
+  it('registers an iban with authorized chars in holder field', async function() {
+    const wallet = await lemonway.Wallet.get(ip, {
+      id: newWallet.id,
+    });
+
+    await wallet.wallet.registerIBAN(ip, {
+      holder: 'alphanumeric 123 and - only',
+      iban: 'FR1420041010050500013M02606'
+    });
+  });
+
+  it("registers an iban with unauthorized chars in 'holder' field replaced by spaces", async function() {
+    const wallet = await lemonway.Wallet.get(ip, {
+      id: newWallet.id,
+    });
+
+    await wallet.wallet.registerIBAN(ip, {
+      holder: 'unauthorized chars like _ and &. for example',
+      iban: 'FR1420041010050500013M02606'
+    });
+
+    const walletWithIban = await lemonway.Wallet.get(ip, {
+      id: newWallet.id,
+    });
+
+    expect(walletWithIban.wallet.ibans.at(-1).holder).to.eql('unauthorized chars like   and    for example');
   });
 });
